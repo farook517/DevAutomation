0bae091f41904b6e0863141b82f3fef99fd44a12
DevAgent <farook517@users.noreply.github.com>
2026-02-15 16:05:09 +0530

    intial changes

---
+++ src/main/java/com/swissre/DevAiApplication.java
@@ -0,0 +1,14 @@
+package com.swissre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAiApplication.class, args);
+		
+	}
+
+}
